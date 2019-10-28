33eff394f582f858636df4bade2e72e665f9c73e
DB <rkdrbgks22@ks.ac.kr>
2019-10-28 19:32:16 +0900

    정렬 문제 1번 풀이
    - 테스트 케이스 1 ~ 7중 2, 6번 실패

---
+++ src/test/java/Solution1Test.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Solution1Test {
+    @Test
+    public void Created(){
+        Solution1 solution1 = new Solution1();
+        assertThat(solution1).isNotNull();
+    }
+    @Test
+    public void Output(){
+        int[] result1 = {5, 6, 3};
+        int[] result2 = {3, 2, 2};
+        Solution1 solution1 = new Solution1();
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        assertThat(solution1.solution(array, commands)).isEqualTo(result1);
+        int[] array2 = {2, 3, 4, 2, 1, 2, 3, 2, 1};
+        int[][] commands2 = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        assertThat(solution1.solution(array2, commands2)).isEqualTo(result2);
+    }
+}
